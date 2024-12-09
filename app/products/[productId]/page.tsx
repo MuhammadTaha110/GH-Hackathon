3e4923ceb666d7596b6469813f7e5bbaf93af2c9
Muhammad Taha Talib <bcsbs2212188@szabist.pk>
2024-12-09 22:39:05 +0500

    V1

---
+++ app/products/[productId]/page.tsx
@@ -0,0 +1,18 @@
+import ProductGrid from "@/components/Home/ProductGrid";
+import { ProductDetails } from "@/components/ProductsDetails/ProductDetails";
+import { ReviewSection } from "@/components/ProductsDetails/ReviewSection";
+import { TabsSection } from "@/components/ProductsDetails/TabsSection";
+import { TOP_SELLING } from "@/lib/constant";
+
+export default function Page() {
+  return (
+    <>
+      <ProductDetails />
+      {/* <TabsSection/> */}
+      <ReviewSection/>
+      <ProductGrid title="YOU MIGHT ALSO LIKE" products={TOP_SELLING} />
+
+
+    </>
+  );
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,11 @@
+// import { ProductDetails } from "@/components/ProductsDetails/ProductDetails";
+// import { ReviewSection } from "@/components/ProductsDetails/ReviewSection";
+
+// export default function Page() {
+//   return (
+//     <>
+//       <ProductDetails />
+//       <ReviewSection/>
+//     </>
+//   );
+// }
